34fdeefe9023c95faaf0d1ffcc7cbbec89df2391
Osinko <yoshinari_m@msn.com>
2014-07-29 23:04:25 +0900

    no message

---
+++ Assets/Lamp.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lamp : MonoBehaviour
+{
+		public Animator anim, monster;
+		public Light blueLight, yellowLight;
+		public float timeSpan;
+	
+		float timeCount;
+		bool blue, yellow, button;
+	
+		void Update ()
+		{
+				timeCount += Time.deltaTime;
+				if (timeCount > timeSpan) {
+						timeCount %= timeSpan;
+						ChangeLamp ();
+				}
+
+				if (Input.GetKeyDown (KeyCode.Space)) {
+						anim.SetTrigger ("elevatorTrig");
+						StartCoroutine (MonsterCrouch ());
+						//メカニムに設定したトリガーをstringで指定して起動する
+						//メカニム側はanyStateではなく新しく自分でつくったNewStateでトラジションを双方につなげ作る
+				}
+		}
+
+		//コルーチンで再生タイムを微調整
+		float startDelay = 0.2f;
+		float crouchStepDelay = 0.8f;
+		public IEnumerator MonsterCrouch ()
+		{
+				WaitForSeconds stdelay = new WaitForSeconds (startDelay);
+				WaitForSeconds delay = new WaitForSeconds (crouchStepDelay);
+				yield return stdelay;
+				monster.SetBool ("crouch", true);
+				yield return delay;
+				monster.SetBool ("crouch", false);
+		
+		}
+
+		void ChangeLamp ()
+		{
+				blue = Random.value > 0.5f ? true : false;
+				yellow = Random.value > 0.5f ? true : false;
+
+				blueLight.enabled = blue;
+				yellowLight.enabled = yellow;
+		}
+
+}
